7346c2fc53610458ca10614e53a752ee8aecdd8d
4r1ss <88276127+4r1ss@users.noreply.github.com>
2022-06-05 14:42:40 +0300

    No need for Username

--- Backup.bat
+++ Backup.bat
@@ -7,16 +7,13 @@ mkdir D:\Backup\Documents
 mkdir D:\Backup\Music
 mkdir D:\Backup\Videos
 mkdir D:\Backup\3D Objects
-
-REM REPLACE C:\Users\"Username"\ WITH YOUR USERNAME
-
-xcopy /s/h/z C:\Users\Username\Pictures D:\Backup\Pictures
-xcopy /s/h/z C:\Users\Username\Desktop D:\Backup\Desktop
-xcopy /s/h/z C:\Users\Username\Downloads D:\Backup\Downloads
-xcopy /s/h/z C:\Users\Username\Documents D:\Backup\Documents
-xcopy /s/h/z C:\Users\Username\Music D:\Backup\Music
-xcopy /s/h/z C:\Users\Username\Videos D:\Backup\Videos
-xcopy /s/h/z C:\Users\Username\3D Objects D:\Backup\3D Objects
+xcopy /s/h/z C:\Users\%USERNAME%\Pictures D:\Backup\Pictures
+xcopy /s/h/z C:\Users\%USERNAME%\Desktop D:\Backup\Desktop
+xcopy /s/h/z C:\Users\%USERNAME%\Downloads D:\Backup\Downloads
+xcopy /s/h/z C:\Users\%USERNAME%\Documents D:\Backup\Documents
+xcopy /s/h/z C:\Users\%USERNAME%\Music D:\Backup\Music
+xcopy /s/h/z C:\Users\%USERNAME%\Videos D:\Backup\Videos
+xcopy /s/h/z C:\Users\%USERNAME%\3D Objects D:\Backup\3D Objects
 
 for /f "tokens=3,2,4 delims=/- " %%x in ("%date%") do set d=%%y%%x%%z
 set data=%d%
